bd9538980f6bc6c5fdfd2c5a8e368a175cbde999
Виктор Кочнев <105153978+laym0n@users.noreply.github.com>
2026-02-15 23:52:20 +0300

    init commit

---
+++ application/src/main/java/com/deposition/application/Application.java
@@ -0,0 +1,16 @@
+package com.deposition.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main Spring Boot application class for the Deposition System.
+ * This is the entry point for the application.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
