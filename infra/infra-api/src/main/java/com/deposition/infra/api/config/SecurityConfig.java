afcf78e18c2f8a495c21b29e1a13a8dd26528518
Виктор Кочнев <105153978+laym0n@users.noreply.github.com>
2026-03-17 01:03:19 +0300

    cors configuration

--- infra/infra-api/src/main/java/com/deposition/infra/api/config/SecurityConfig.java
+++ infra/infra-api/src/main/java/com/deposition/infra/api/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.deposition.infra.api.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -8,24 +10,43 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.CorsUtils;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-        @Bean
-        public SecurityFilterChain filterChain(HttpSecurity http,
-                        Customizer<OAuth2ResourceServerConfigurer<HttpSecurity>> oAuth2ResourceServerCustomizer)
-                        throws Exception {
-                http
-                                .authorizeHttpRequests(authz -> authz
-                                                .requestMatchers("/actuator/**", "/swagger-ui/**", "/v3/api-docs/**",
-                                                                "/swagger-ui.html")
-                                                .permitAll()
-                                                .anyRequest().authenticated())
-                                .oauth2ResourceServer(oAuth2ResourceServerCustomizer)
-                                .csrf(AbstractHttpConfigurer::disable);
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http,
+            Customizer<OAuth2ResourceServerConfigurer<HttpSecurity>> oAuth2ResourceServerCustomizer)
+            throws Exception {
+        http
+                .authorizeHttpRequests(authz -> authz
+                .requestMatchers("/actuator/**", "/swagger-ui/**", "/v3/api-docs/**",
+                        "/swagger-ui.html")
+                .permitAll()
+                .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
+                .anyRequest().authenticated())
+                .oauth2ResourceServer(oAuth2ResourceServerCustomizer)
+                .cors(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable);
 
-                return http.build();
-        }
+        return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("*"));
+        config.setAllowedMethods(List.of("*"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(false);
+
+        var source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
 }
